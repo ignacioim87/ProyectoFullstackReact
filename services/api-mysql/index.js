24793bfb4690c98da31852173987f94b081bf417
Ignacio Montaña <ignaciom8719@gmail.com>
2022-08-24 12:27:52 -0300

    fix: baseurl changed to heroku url

--- services/api-mysql/index.js
+++ services/api-mysql/index.js
@@ -4,7 +4,7 @@ class DatabaseService   {
 
     constructor() {
         this.client = axios.create({
-            baseURL: "http://localhost:3002/api/v1/"
+            baseURL: "us-cdbr-east-06.cleardb.net"
         })
     }
 
